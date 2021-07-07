08a9b10548bb579c4f61e561bf2dd6247a5e89b8
PeppermillDetective <peppermilldetective@gmail.com>
2021-07-06 22:30:43 -0500

    Fix typo

--- lua/settings.lua
+++ lua/settings.lua
@@ -231,7 +231,7 @@ require('telescope').setup{
     initial_mode = "insert",
     selection_strategy = "reset",
     sorting_strategy = "descending",
-    layout_strategy = "horiontal",
+    layout_strategy = "horizontal",
     layout_config = {
       horizontal = {
         mirror = false,
